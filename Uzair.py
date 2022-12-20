0c270eceb0091d5fd8f82f4a024ebdb908029cc7
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2022-12-20 14:53:53 +0545

    Update Uzair.py

--- Uzair.py
+++ Uzair.py
@@ -1,8 +1,7 @@
 import os 
 os.system('git pull')
 os.system('termux-setup-storage')
-print("\033(1.32m welcome to my tool
-	you can easily cloning our tool,but main thing you need creat best dump file for many idz cloning ")
+print("\033(1.32m    welcome to my tool you can easily cloning our tool,but main thing you need creat best dump file for many idz cloning ")
 import ahad
 Main()
 
