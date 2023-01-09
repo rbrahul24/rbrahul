376cb371931e27d1a419eaee8111125496faeadf
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-01-09 10:44:55 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -74,7 +74,6 @@ tokenku=[]
 
 ###---[ AUTO CREATE UA & PROXY ]---###
 try:
-	clear_layar()
 	print(logo2())
 	print(f'\r\n [{hh}>{P}] sedang dump proxy dan create useragent')
 	try:os.remove('.proxy.txt')
