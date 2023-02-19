a3e8724d57c0e58f3be4496ddd4316e85af113d3
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-02-19 22:08:35 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -86,9 +86,10 @@ def approval():
       input('IF U WANT TO BUY THEN PRESS ENTER ')
       tks = ('Hello%20Sir%20!%20Please%20Approve%20My%20Token%20The%20Token%20Is%20:%20'+id);os.system('am start https://wa.me/+923188508994?text='+tks),approval()
       time.sleep(1)
+      approval()
   except:
     sys.exit()
-        approval()
+        
 def clear():
         os.system('clear')
         print(logo)
