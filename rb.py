fa094e2b0588dc5a17e63a648fa42fab6c7b48ff
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-02-19 13:55:42 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -60,7 +60,23 @@ logo = ("""\033[1;32m
 \033[1;37m----------------------------------------------""")
 def linex():
         print('\033[1;37m----------------------------------------------')
+# CLEAR
+def clear():
+	os.system('clear')
+# BACK
+def back():
+	login()
 
+rahul="RAHUL-"
+imt="-M4786=="
+ak="RAHUL-"
+myid=uuid.uuid4().hex[:10].upper()
+try:
+	key1 = open('/data/data/com.termux/files/usr/bin/.mrrahul-cov', 'r').read()
+except:
+	kok=open('/data/data/com.termux/files/usr/bin/.mrrahul-cov', 'w')
+	kok.write(myid+imt)
+	kok.close()
 def clear():
         os.system('clear')
         print(logo)
@@ -426,6 +442,40 @@ def api1(ids,names,passlist):
                         time.sleep(10)
                 except Exception as e:
                         pass
+def Subscraption():
+	key1=open('/data/data/com.termux/files/usr/bin/.mrrahul-cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://raw.githubusercontent.com/Hackerrv33/rahulrbc/main/server.txt").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m RAHUL Tool Paid You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m Note : Paid Tools HA JANI LOG \033[1;37m")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+ak+rahul+key1)
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+rahul+''+key1
+		os.system('am start https://wa.me/+918736899399?text=' + tks)
+		
+Subscraption()        
 try:
      
         menu()
@@ -434,4 +484,3 @@ except requests.exceptions.ConnectionError:
         exit()
 except Exception as e:pass
 menu()
-
