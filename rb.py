5c64d245c16bd278d32f5966240ca66c1381f583
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-02-26 12:18:28 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -69,7 +69,7 @@ def approval():
       time.sleep(1)
       approval()
   except:
-    sys.exit()        
+    os.system('python RAHUL.py')     
 logo = ("""\033[1;32m      
   ____  _       ____       _           _ 
  |  _ \| |__   |  _ \ __ _| |__  _   _| |
