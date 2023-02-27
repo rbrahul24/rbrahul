46900e1d89977ae999ae5b4f1a257eade43b8002
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-02-27 18:00:14 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -144,7 +144,7 @@ def remove_dub():
         print('\n\033[0;97m File not found on provided path, try again ...\033[0;97m')
 def menu():
         try:
-                approval()
+                clear()
         #       chk()
                 x = ("sex")
                 if x == ("sex"):
