32e1266eb2d8a42d7263d984d1476f293fff819c
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-03-05 09:00:09 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -532,7 +532,7 @@ def api3(ids,names,passlist):
                                         'x-fb-friendly-name':'authenticate',
                                         'accept-encoding':'gzip, deflate',
                                         'x-fb-http-engine':     'Liger'}
-                                url = 'https://b-graph.facebook.com/method/auth/login'
+                                url = 'https://b-graph.facebook.com/auth/login?include_headers=false&decode_body_json=false&streamable_json_response=true'
                                 po = requests.post(url,data=data,headers=head,allow_redirects=False).text
                                 q = json.loads(po)
                                 if 'session_key' in q:
